42cdfd5cc350cb2894dd9c2997cf83477be66fcd
Hany El Nokaly <hany.elnokaly@gmail.com>
2022-07-02 18:05:37 +0200

    fix

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,5 +1,5 @@
-eb init udagram-api --platform node.js-14 --region us-east-1 --vpc.id $AWS_VPC_ID
-eb create udagram-api
+eb init udagram-api --platform node.js-14 --region us-east-1
+eb create udagram-api --vpc.id $AWS_VPC_ID
 eb list
 eb use udagram-api
 eb setenv PORT=$PORT POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_HOST=$POSTGRES_HOST POSTGRES_DB=$POSTGRES_DB JWT_SECRET=$JWT_SECRET AWS_BUCKET=$AWS_BUCKET AWS_REGION=$AWS_REGION AWS_PROFILE=$AWS_PROFILE
