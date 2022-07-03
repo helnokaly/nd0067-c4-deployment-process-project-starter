48b2b578ccf087488124dc5f41c8a4216c749b81
Hany El Nokaly <hany.elnokaly@gmail.com>
2022-07-04 01:25:38 +0200

    change apiHost

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api.eba-aetn4f9j.us-east-1.elasticbeanstalk.com:3000/api/v0',
+  apiHost: 'http://udagram-api.eba-aetn4f9j.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
